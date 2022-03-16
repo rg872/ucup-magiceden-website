ce11598c5edde347fd5935eca21ed80673962a21
Rafie Gilang <gilang0293@gmail.com>
2022-03-16 21:08:50 +0700

    add contentsecutyrypolicy

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     domains: ["www.skullbots.io"],
     loader: "custom",
   },
